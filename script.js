20e62ddfc601ac279887ac3a0af3099d389c944a
Rhiannon Liu <rhiannon0712@gmail.com>
2025-07-15 22:38:41 -0700

    Reorganize addInput function & add remove function

--- script.js
+++ script.js
@@ -6,10 +6,24 @@ function addInput() {
     const inputs = document.getElementById('localInputs');
 
     const label = document.createElement('label');
-    label.textContent = `Location ${localCounter}`;
+    label.textContent = `Location ${localCounter}:`;
 
     const inputBox = document.createElement('input');
+
+    const button = document.createElement('button');
+    button.type = "button" /*doesn't submit the form once clicked on*/
+    button.textContent = "-"
+    button.onclick = function() {
+        label.remove();
+        inputBox.remove();
+        button.remove();
+        localCounter--;
+    }
+
+    const br = document.createElement('br');
     
     inputs.appendChild(label);
+    inputs.appendChild(br);
     inputs.appendChild(inputBox);
+    inputs.appendChild(br);
 }
