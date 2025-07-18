c0bbb25c86f095d1d62c9b6c4d62e26056cc7bea
Rhiannon Liu <rhiannon0712@gmail.com>
2025-07-17 19:13:04 -0700

    Changed styling - locations are no longer numbered

--- script.js
+++ script.js
@@ -1,34 +1,24 @@
-var localCounter = 2
+function addLoca() {
 
-function addInput() {
-    localCounter++;
+    const locaInputs = document.getElementById('locaInputs');
 
-    const inputs = document.getElementById('localInputs');
-
-    const div = document.createElement('div');
-
-    const label = document.createElement('label');
-    label.textContent = `Location ${localCounter}:`;
-
-    const inputBox = document.createElement('input');
+    const wrapper = document.createElement('div');
+    
+    const locaInput = document.createElement('input')
+    locaInput.type = "search";
+    locaInput.name = "locaInput"
+    locaInput.placeholder = "Enter Location";
 
     const button = document.createElement('button');
-    button.type = "button" /*doesn't submit the form once clicked on*/
-    button.textContent = "-";
     button.style.backgroundColor = "rgb(92, 123, 19)";
     button.style.color = "white";
+    button.textContent = "-"
     button.onclick = function() {
-        label.remove();
-        inputBox.remove();
-        button.remove();
-        localCounter--;
+        wrapper.remove();
     }
-    
-    div.appendChild(label);
-    div.appendChild(document.createElement('br'));
-    div.appendChild(inputBox);
-    div.appendChild(button);
-    div.appendChild(document.createElement('br'));
 
-    inputs.appendChild(div);
+    wrapper.appendChild(locaInput);
+    wrapper.appendChild(button);
+    locaInputs.appendChild(wrapper);
 }
+
