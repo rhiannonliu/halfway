9443f7941f0611df582d50ae8844d7ee6ba6fcb4
Rhiannon Liu <rhiannon0712@gmail.com>
2025-07-19 20:14:44 -0700

    deleted adding multiple locations function

--- script.js
+++ script.js
@@ -1,24 +1,2 @@
-function addLoca() {
 
-    const locaInputs = document.getElementById('locaInputs');
-
-    const wrapper = document.createElement('div');
-    
-    const locaInput = document.createElement('input')
-    locaInput.type = "search";
-    locaInput.name = "locaInput"
-    locaInput.placeholder = "Enter Location";
-
-    const button = document.createElement('button');
-    button.style.backgroundColor = "rgb(92, 123, 19)";
-    button.style.color = "white";
-    button.textContent = "-"
-    button.onclick = function() {
-        wrapper.remove();
-    }
-
-    wrapper.appendChild(locaInput);
-    wrapper.appendChild(button);
-    locaInputs.appendChild(wrapper);
-}
 
